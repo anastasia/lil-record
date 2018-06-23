2d664b636ac64329b0e86a71e21a4d286583efa0
anastasia <anastasia.aizman@gmail.com>
2018-06-23 16:36:37 -0400

    add - storage functionality (moving, writing files), api endpoints

--- web/storage.py
+++ web/storage.py
@@ -8,14 +8,16 @@ s3 = boto3.client(
    aws_secret_access_key=S3_SECRET)
 
 
-def upload(filepath, filename, folder="", bucket_name=S3_BUCKET):
+def upload(local_filepath, folder="", bucket_name=S3_BUCKET):
     """
     Upload to s3. Allows programmatic creation of subfolders in bucket
     """
+    # import pdb; pdb.set_trace()
+    filename = local_filepath.split('/')[-1]
     uploaded_filepath = os.path.join(folder, filename)
     try:
         s3.upload_file(
-            filepath,
+            local_filepath,
             bucket_name,
             uploaded_filepath)
 
@@ -24,3 +26,58 @@ def upload(filepath, filename, folder="", bucket_name=S3_BUCKET):
         return e
 
     return "{}{}".format(S3_LOCATION, uploaded_filepath)
+
+
+def get_contents(directory, bucket_name=S3_BUCKET):
+    result = s3.list_objects(
+        Bucket=bucket_name)
+    contents = result.get('Contents')
+    r = []
+    for f in contents:
+        d, fn = f.get('Key').split('/')
+        if d == directory:
+            r.append(f)
+    return r
+
+
+def download_archived(filename, type_of_file, bucket_name=S3_BUCKET):
+    if type_of_file != "current":
+        filepath = os.path.join("%ss" % type_of_file, filename)
+    else:
+        filepath = os.path.join("%ss" % type_of_file, filename)
+    return download(filepath)
+
+
+def download(s3_filepath, local_filename, bucket_name=S3_BUCKET):
+    s3.download_file(
+        bucket_name,
+        s3_filepath,
+        local_filename)
+    return local_filename
+
+
+def download_current_questions():
+    dircontents = get_contents("current")
+    questions = {}
+    for file_obj in dircontents:
+        name = file_obj['Key'].split('/')[-1]
+        q = download(file_obj['Key'], name)
+        with open(q, "r") as f:
+            questions[file_obj['Key']] = f.read()
+
+    return questions
+
+
+def move_file(src, dest):
+    print("moving file", src, dest)
+    copy_source = {
+        'Bucket': S3_BUCKET,
+        'Key': src}
+    print("copying questions")
+    s3.copy(copy_source, S3_BUCKET, dest)
+    print("delete question", src)
+    s3.delete_object(Bucket=S3_BUCKET, Key=src)
+    return dest
+
+
+
--- web/views.py
+++ web/views.py
@@ -1,7 +1,11 @@
-from flask import Flask, Response
+import os
+import time
+from flask import Flask, Response, request
+
+from web.storage import move_file, download_current_questions, upload as s3_upload
+import settings
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def hello():
@@ -9,7 +13,48 @@ def hello():
 
 @app.route("/questions")
 def questions_list():
+
     a_list = ["question_1", "question_2", "question_3"]
     return Response(a_list)
 
+
+@app.route("/upload/<type_of_file>/<filename>")
+def upload(type_of_file, filename):
+    """
+    uploads to either answers or questions to s3
+    """
+    filedir = os.path.join(settings.RECORDINGS_DIR, "%ss" % type_of_file)
+    filepath = os.path.join(filedir, filename)
+
+    s3_upload(filepath, filename, folder=type_of_file)
+    return Response("ok")
+
+
+@app.route("/change_current/<question_number>", methods=["POST"])
+def change_current(question_number):
+    """
+    post new question to question number
+    curl --data "data=New question?" http://lilrecord.website/change_current/2
+    """
+    millis = int(round(time.time() * 1000))
+    filename = "%s_%s.txt" % (question_number, millis)
+    local_filepath = "/tmp/%s" % filename
+    if not os.path.isdir('/tmp'):
+        os.mkdir('/tmp')
+    with open(local_filepath, "w+") as f:
+        f.write(request.form['data'])
+    questions = download_current_questions()
+    for question in questions:
+        folder, path = question.split('/')
+        number, rest_of_path = path.split('_')
+        if number == question_number:
+            # move question to archive
+            move_file(question, "%s/%s" % ("questions", path))
+
+    s3_upload(local_filepath, folder="current")
+
+    # delete file locally
+    os.remove(local_filepath)
+
+    return Response("ok")
 
