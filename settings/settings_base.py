706a7a85bf70644c929d71e4e7b907db59ea00a8
anastasia <anastasia.aizman@gmail.com>
2018-06-24 01:33:54 -0400

    add - slack integration

--- settings/settings_base.py
+++ settings/settings_base.py
@@ -2,3 +2,8 @@ S3_BUCKET = "lil-record"
 S3_KEY = "secret_key"
 S3_SECRET = "secret_secret"
 S3_LOCATION = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
+
+SLACK_API_TOKEN = "slack-token"
+SLACK_CHANNEL_ID = "test-channel"
+SLACK_BOT_NAME = "lil-record"
+SLACK_BOT_ID = "bot-id"
---
+++ slack/slack.py
@@ -0,0 +1,65 @@
+import requests
+import json
+from slackclient import SlackClient
+from settings import SLACK_API_TOKEN, SLACK_CHANNEL_ID, SLACK_BOT_ID, SLACK_BOT_NAME
+
+available_commands = """
+*Available Commands*
+list questions - get a list of questions \n
+list answers <number> - get a list of answers to current question number \n
+change question <number> <question> - replace question at number \n
+help - get this message
+"""
+
+sc = SlackClient(SLACK_API_TOKEN)
+# if slack.sc.rtm_connect():
+#     print('connected')
+#     while True:
+#         slack.check_messages()
+#         time.sleep(1)
+
+
+def post(message, channel=SLACK_CHANNEL_ID):
+    sc.api_call(
+      "chat.postMessage",
+      channel=channel,
+      text=message)
+
+
+def check_messages():
+    messages = sc.rtm_read()
+    for m in messages:
+        print(m.get('text', ''))
+        if "<@%s>" % SLACK_BOT_ID not in m.get('text', ''):
+            return
+
+        kwargs = {
+            'channel': m.get('channel', SLACK_CHANNEL_ID),
+            'username': SLACK_BOT_NAME,
+            'unfurl_links': True,
+            'icon_emoji': ":phone:"}
+
+        command = m.get('text', '').split("<@%s> " % SLACK_BOT_ID)[1]
+        if  "list" in command:
+            if command == "list questions":
+                res = requests.get("http://localhost:5000/questions")
+                questions = json.loads(res.text)
+                q_str = ""
+                for q in questions:
+                    q_str += "*%s* - %s\n" % (q, questions[q])
+                sc.api_call("chat.postMessage", text=q_str, **kwargs)
+            # elif "list answers" in command:
+            #     res = requests.get("http://localhost:5000/answers")
+            #     questions = json.loads(res.text)
+            #     q_str = ""
+            #     for q in questions:
+            #         q_str += "*%s* - %s\n" % (q, questions[q])
+            #     sc.api_call("chat.postMessage", text=q_str, **kwargs)
+        elif "change question" in command:
+            command_parts = command.split(' ')
+            number = command_parts[2]
+            new_question = " ".join(command_parts[3:])
+            response = requests.post("http://localhost:5000/change_current/%s" % number, json={'data':new_question})
+            sc.api_call("chat.postMessage", text=response.text, **kwargs)
+        elif command == "help":
+            sc.api_call("chat.postMessage", text=available_commands, **kwargs)
