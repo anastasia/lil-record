97afb30990d442980f945360597462c7f10b43d0
anastasia <anastasia.aizman@gmail.com>
2018-08-10 15:45:59 -0400

    update - gitignore and add credentials

--- load_questions.sh
+++ load_questions.sh
@@ -1,2 +1,3 @@
 cd /home/pi/lil-record
+export GOOGLE_APPLICATION_CREDENTIALS=/home/pi/lil-record/settings/keys/lil-record.json
 export PYTHONPATH=$PYTHONPATH:$PWD ; python3 record/load_questions.py
